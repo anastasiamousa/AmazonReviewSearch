2b1a1e5e33181a255e07e7008ef5f063cd0b9c5a
anastasiamousa <anastasiamousa34@gmail.com>
2025-01-05 13:59:57 +0200

    Initial Commit

---
+++ AmazonReviewSearch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AmazonReviewSearch
+//
+//  Created by Anastasia Mousa on 5/1/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
