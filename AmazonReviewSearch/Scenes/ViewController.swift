f379486f29d03949a328f464f8e6f8b60d81f5b6
anastasiamousa <anastasiamousa34@gmail.com>
2025-01-08 02:24:17 +0200

    storyboard additions

--- AmazonReviewSearch/Scenes/ViewController.swift
+++ AmazonReviewSearch/Scenes/ViewController.swift
@@ -9,9 +9,19 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var titleLabel: NSTextFieldCell!
+    
+    @IBOutlet weak var headerView: NSTableHeaderView!
+    
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let label = NSTextField(labelWithString: "Search results")
+        self.headerView.addSubview(label)
     }
 
     override var representedObject: Any? {
